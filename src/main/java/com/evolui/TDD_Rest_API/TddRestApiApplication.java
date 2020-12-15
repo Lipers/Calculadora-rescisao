566cb871be5c6510fcb63862929f44a0ff550c7c
Felipe Souza <lipers20@hotmail.com>
2020-12-15 15:52:18 -0300

    0.1 - spring

---
+++ src/main/java/com/evolui/TDD_Rest_API/TddRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.evolui.TDD_Rest_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddRestApiApplication.class, args);
+	}
+
+}
